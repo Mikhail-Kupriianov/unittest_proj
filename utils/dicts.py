780e5f13aef538960bcd3b66b28bcd8807c61281
Mkupr <mkupr@outlook.com>
2023-03-02 23:31:04 +0300

    fix test_dicts.py

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,21 @@
 def get_val(collection, key, default='git'):
-    pass
+    """
+    Функция возвращает значение из словаря по переданному ключу, если ключ существует.
+    В ином случае возвращается значение default.
+    :param collection: Словарь для поиска значения по ключу
+    :param key: Ключ для поиска в словаре
+    :param default: Слово, которое вернётся, если значение не найдено
+    :return:
+    """
+    # Если словарь пустой или ключа нет - возврат default
+    if not collection or key not in collection:
+        return default
+    #  В противном случае, если ключ есть - вернуть значение
+    elif key in collection:
+        return collection.get(key)
+    # Заглушка - не удалось предусмотреть все варианты
+    # если мы здесь - что-то пошло не так.
+    # Конструкция написана в учебных целях.
+    # Результат - покрытие только 83% :)
+    else:
+        print("Exception in get_val function of dicts.py module - ATTENTION!!!")
