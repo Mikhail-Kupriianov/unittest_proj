2700bbf190e7d1cf855256748039285892a3c1d2
Mkupr <mkupr@outlook.com>
2023-03-02 21:47:58 +0300

    fix test_dict.py

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -4,10 +4,8 @@ from utils import dicts
 
 class TestDict(unittest.TestCase):
 
-
     def test_get(self):
         self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs"), "mercurial")
         self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs", "git"), "mercurial")
         self.assertEqual(dicts.get_val({}, "vcs", "git"), "git")
         self.assertEqual(dicts.get_val({}, "vcs", "bazaar"), "bazaar")
-
